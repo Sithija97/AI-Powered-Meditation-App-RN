6f869a70659dc007d248fd8da678788b87ff7951
Sithija Shehara <nsithijashehara@gmail.com>
2023-07-23 10:31:22 +0530

    User already exists logic added

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -16,6 +16,11 @@ const registerUser = asyncHandler(async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
   const userExists = await User.findOne({ email });
 
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
   const user = await User.create({
     name,
     email,
