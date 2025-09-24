095e358c8949ebcfdee851b298e7575334e8c9ad
Sithija Shehara <nsithijashehara@gmail.com>
2025-09-25 00:25:15 +0530

    initial app setup and clerk config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,47 @@
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Stack } from "expo-router";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
 
-export default function RootLayout() {
+function RootLayoutWithAuth() {
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
+    <Stack>
+      <Stack.Protected guard={isSignedIn}>
+        <Stack.Screen name="(protected)" />
+      </Stack.Protected>
+      <Stack.Protected guard={!isSignedIn}>
+        <Stack.Screen name="(public)" />
+      </Stack.Protected>
     </Stack>
   );
 }
+
+export default function RootLayout() {
+  console.log(`key : ${process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}`);
+  return (
+    <ClerkProvider
+      tokenCache={tokenCache}
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+    >
+      <RootLayoutWithAuth />
+    </ClerkProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- shared/colors.tsx
+++
@@ -1,6 +0,0 @@
-export default {
-  PRIMARY: "#6C0BF4",
-  BLACK: "#000000",
-  WHITE: "#FFFFFF",
-  GRAY: "#808080",
-};
